e22f6bc0346e1e798a20e470191b97ecde7567a1
NetFeez <netfeez.dev@gmail.com>
2026-05-01 06:25:20 +0000

    refactor: bin disable auto configure .vscode/settings.json temporally

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
+import { File } from "@netfeez/common-node";
 
 import DepFlowCLI from "./DepFLowCLI.js";
 import { Utils } from "../support/Utils.js";
 import Schemas from "../config/schemas.js";
 
-function startupSchemaValidation(flowPath: string) {
+async function startupSchemaValidation(flowPath: string) {
     const jsonSchema = Schemas.Config.jsonSchema;
+    // Temporarily write the schema to the .depflow directory for validation purposes
+    // The vscode configuration was out for now, while ill enhance it we will use the $schema property in the depflow.json file to point to this schema
+    File.write(`.depflow/schema.json`, JSON.stringify(jsonSchema));
     // Utils.addVscodeValidation(flowPath, jsonSchema);
 }
 
@@ -21,7 +25,7 @@ const flowPath = flowTag[0] || 'depflow.json';
 
 const cli = new DepFlowCLI(flowPath);
 
-startupSchemaValidation(flowPath);
+await startupSchemaValidation(flowPath);
 
 try {
     if (commandName !== null) {
