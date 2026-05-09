4b7b487adad1e3f59cc553a859af49572b2afb2b
NetFeez <netfeez.dev@gmail.com>
2026-05-09 19:56:04 +0000

    feat: enhance extraction process with dynamic path and string replacement functionality

--- src/support/Builder/Builder.ts
+++ src/support/Builder/Builder.ts
@@ -46,16 +46,20 @@ export class Builder {
         const extractor = typeof entry === 'string' ? [ { from: '**/*', to: entry } ] : entry;
 
         for (const entry of extractor) {
-            const { from, to: toEntry, replacer } = entry;
+            const { from, to: toEntry, pathReplacer, replacer } = entry;
 
-            const regex = !replacer ? null : new RegExp(typeof replacer === 'string' ? replacer : replacer.search, 'g');
-            const value = !replacer || typeof replacer === 'string' ? '' : replacer.replace;
+            const pathReplacerAction = Builder.replacer(pathReplacer);
+            const replacerAction = Builder.replacer(replacer);
 
             const to = Path.isAbsolute(toEntry) ? toEntry : Path.join(Path.cwd, toEntry);
             this.logger?.log(`&C7Extracting &C3${from}&C7 to &C3${to}&C7...`);
-            await File.smartCopy(from, to, {
+            await File.smartProcess(from, to, {
                 cwd: this.cwd,
-                map: path => regex ? path.replace(regex, value) : path
+                map: pathReplacerAction,
+            }, async ({ src, dest }) => {
+                const content = await File.read(src);
+                const transformedContent = replacerAction(content);
+                File.write(dest, transformedContent);
             });
         }
     }
@@ -81,8 +85,26 @@ export class Builder {
             return () => { pollito.stop(); };
         }, maxTimeMs);
     }
+    /**
+     * Creates a string replacement function based on the provided replacer configuration, which can be either a simple string or an object defining a search pattern and replacement string. This method is used to generate a function that can be applied to strings for dynamic transformations during the build process, allowing for flexible and customizable string manipulation as defined in the builder configuration.
+     * @param replacer A configuration for string replacement, which can be either a string (used as a regex pattern) or an object containing a search pattern and a replacement string.
+     * @returns A function that takes a string as input and returns a new string with the specified replacements applied, based on the provided replacer configuration.
+     * @throws Will throw an error if the provided replacer configuration is invalid, such as if it is neither a string nor an object with the required properties.
+     */
+    protected static replacer(replacer: Schemas.Replacer['infer']): Builder.ReplacerAction {
+        if (typeof replacer === 'string') {
+            return (str) => str.replace(new RegExp(replacer, 'g'), '');
+        } else if (replacer !== null && typeof replacer === 'object') {
+            const flags = replacer.flags || 'g';
+            const search = new RegExp(replacer.search, flags);
+            const replace = replacer.replace || '';
+            return (str) => str.replace(search, replace);
+        } else return (str) => str;
+    }
 }
 export namespace Builder {
+    export type Replacer = Schemas.Replacer['infer'];
+    export type ReplacerAction = (str: string) => string;
     export type BuilderEntry = Schemas.BuilderEntry['infer'];
     export type Extractor = Schemas.ExtractorEntry['infer'];
     export interface Info {
